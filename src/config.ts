f763c4636488aeea5ffc7402e2c5044cfef040ec
Angelo Silva <angelo.lambda@gmail.com>
2020-09-11 14:52:55 -0300

    Adiciona prefetch nas variáveis de ambiente

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 export const rabbitHost = process.env.RABBIT_HOST;
 export const elasticHost = process.env.ELASTIC_HOST;
 export const token = process.env.TOKEN;
-export const prefetch = process.env.PREFETCH || 50;
+export const prefetch = Number(process.env.PREFETCH || 50);
